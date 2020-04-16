89b5ca2815052aa4305a36ae0db2da3677980d6e
Rob Marti <rob@linuxacademy.com>
2020-04-16 11:59:12 -0500

    Fixed filename typo

--- build-forward.sh
+++ build-forward.sh
@@ -1,4 +1,4 @@
-cat  <<EOF > /var/named/named.conf
+cat  <<EOF > /var/named/forward.db
 \$TTL    86400
 @       IN      SOA     nameserver.mylabserver.com. root.mylabserver.com. (
                           10030         ; Serial
@@ -12,20 +12,20 @@ cat  <<EOF > /var/named/named.conf
 ; A Record Definitions
 nameserver  IN      A       
 EOF
-truncate -s -1 /var/named/named.conf
-ifconfig eth0 | grep "inet " | awk -F ' ' '{ print $2 }' >> /var/named/named.conf
-cat <<EOF >> /var/named/named.conf
+truncate -s -1 /var/named/forward.db
+ifconfig eth0 | grep "inet " | awk -F ' ' '{ print $2 }' >> /var/named/forward.db
+cat <<EOF >> /var/named/forward.db
 test1       IN      A       
 EOF
-truncate -s -1 /var/named/named.conf
-ifconfig eth0 | grep "inet " | awk -F ' ' '{ print $2 }' | cut -f1-3 -d'.' | tr -d '\n' >> /var/named/named.conf
-cat <<EOF >> /var/named/named.conf
+truncate -s -1 /var/named/forward.db
+ifconfig eth0 | grep "inet " | awk -F ' ' '{ print $2 }' | cut -f1-3 -d'.' | tr -d '\n' >> /var/named/forward.db
+cat <<EOF >> /var/named/forward.db
 .30
 test2       IN      A       
 EOF
-truncate -s -1 /var/named/named.conf
-ifconfig eth0 | grep "inet " | awk -F ' ' '{ print $2 }' | cut -f1-3 -d'.' | tr -d '\n' >> /var/named/named.conf
-cat <<EOF >> /var/named/named.conf
+truncate -s -1 /var/named/forward.db
+ifconfig eth0 | grep "inet " | awk -F ' ' '{ print $2 }' | cut -f1-3 -d'.' | tr -d '\n' >> /var/named/forward.db
+cat <<EOF >> /var/named/forward.db
 .72
 EOF
 
