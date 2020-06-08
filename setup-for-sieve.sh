0de3e81957d15a53082d29b5a679a016f39f210d
Rob Marti <rob@linuxacademy.com>
2020-06-08 00:38:31 -0500

    Fixed Typo

--- setup-for-sieve.sh
+++ setup-for-sieve.sh
@@ -11,13 +11,15 @@ sed -ibak -e 's;#mynetworks = 168.100.189.0/28, 127.0.0.0/8;mynetworks = 10.0.0.
 sed -ibak -e 's;#home_mailbox = Maildir/;home_mailbox = Maildir/;' /etc/postfix/main.cf
 sed -ibak -e 's;mydestination = $myhostname, localhost.$mydomain, localhost;mydestination = $myhostname, localhost.$mydomain, localhost, $mydomain;' /etc/postfix/main.cf
 restorecon /etc/postfix/main.cf
+systemctl restart postfix
 
-sed -ibak -e 's;#mail_location = maildir:~/Maildir;mail_location = maildir:~/Maildir;' /etc/dovecot/conf.d/10-mail.conf
+sed -ibak -e 's;#   mail_location = maildir:~/Maildir;mail_location = maildir:~/Maildir;' /etc/dovecot/conf.d/10-mail.conf
 sed -ibak -e 's;auth_mechanisms = plain;auth_mechanisms = plain login;' /etc/dovecot/conf.d/10-mail.conf
+systemctl restart dovecot 
 
 useradd test_user
 /bin/echo '1234' | /bin/passwd test_user --stdin
 mkdir /home/test_user/Maildir
 chown test_user.test_user /home/test_user/Maildir/
 chmod -R 700 /home/test_user/Maildir/
-
+doveadm mailbox create -u test_user Work
